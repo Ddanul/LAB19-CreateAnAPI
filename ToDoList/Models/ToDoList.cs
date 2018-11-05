4448b60e71bce5419e916cc1db91172b2601e9c0
Danul De Leon <danul.deleon@gmail.com>
2018-11-04 22:51:24 -0800

    trying to set up

---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoList
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<ToDoItem> ToDoItem { get; set; }
+    }
+}
